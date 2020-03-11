12295e75b482558c2be179ff198d37199e1cc6bb
kang hee yong <cagojeiger@naver.com>
2020-03-11 20:29:05 +0900

    add non-async

--- Feynman/etc/try_sync_access.py
+++ Feynman/etc/try_sync_access.py
@@ -1,4 +1,5 @@
 import os
+import time
 import fcntl
 
 import asyncio
@@ -10,6 +11,24 @@ class Try_sync_access:
     def __init__(self, fname):
         self.fname = fname
         self.logger = get_logger('try_sync_access')
+        self.sleep_t = 2
+
+    def __enter__(self):
+        while True:
+            try:
+                fd = os.open(self.fname, os.O_CREAT)
+                fcntl.flock(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                self.fd = fd
+                break
+            except OSError:
+                os.close(fd)
+                self.logger.info('Waiting to release {}'.format(self.fname))
+                time.sleep(self.sleep_t)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # fcntl.flock(self.fd, fcntl.LOCK_UN)
+        os.remove(self.fname)
+        os.close(self.fd)
 
     async def __aenter__(self):
         while True:
@@ -21,7 +40,7 @@ class Try_sync_access:
             except OSError:
                 os.close(fd)
                 self.logger.info('Waiting to release {}'.format(self.fname))
-                await asyncio.sleep(5)
+                await asyncio.sleep(self.sleep_t)
 
     async def __aexit__(self, exc_type, exc_value, traceback):
         # fcntl.flock(self.fd, fcntl.LOCK_UN)

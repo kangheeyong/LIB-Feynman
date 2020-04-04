846a889addaa4d93f17b78e3038685a871f0f7d5
kang hee yong <cagojeiger@naver.com>
2020-04-04 19:42:33 +0900

    add prototype

---
+++ Feynman/templete/templete_manager.py
@@ -0,0 +1,44 @@
+import time
+import asyncio
+import websockets
+
+from fire import Fire
+
+from Feynman.etc.util import get_logger
+
+
+class templete_manager():
+    def __init__(self):
+        self.logger = get_logger()
+
+    async def _task(self):
+        self.logger.info('Start task...')
+        while True:
+            begin_t = time.time()
+            print('tesk test')
+            sleep_t = max(0, 60 - int(time.time() - begin_t))
+            self.logger.info('Sleep {} secs before next start'.format(sleep_t))
+            await asyncio.sleep(sleep_t)
+
+    async def _consumer(self, websocket, path):
+        self.logger.info('Start consumer... at {}:{}{}'.format(websocket.host, websocket.port, path))
+        name = await websocket.recv()
+
+        print('< {}'.format(name))
+        greeting = 'Hello {}!'.format(name)
+
+        await websocket.send(greeting)
+        print('> {}'.format(greeting))
+
+    async def _main(self):
+        coroutine_list = []
+        coroutine_list.append(websockets.serve(self._consumer, '0.0.0.0', 8765))
+        coroutine_list.append(self._task())
+        await asyncio.gather(*coroutine_list)
+
+    def run(self):
+        asyncio.run(self._main())
+
+
+if __name__ == '__main__':
+    Fire(templete_manager)
